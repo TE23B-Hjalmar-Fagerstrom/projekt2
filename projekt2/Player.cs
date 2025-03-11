521c7ba22e91d2257be85c3f9c4ad097ad0864fb
Hjalmar Fagerström <hjalmar.fagerstrom@elev.ga.ntig.se>
2025-03-11 13:23:54 +0100

    började skriva om spelar klassen

---
+++ projekt2/Player.cs
@@ -0,0 +1,18 @@
+namespace Player
+{
+    class player
+    {
+        public int playerHealth = 50;
+        public int playerDamage = 1;
+        public int playerGold = 15;
+        public int playerGoldSpent = 0;
+    }
+
+    class character
+    {
+        public int characterHealth = 100;
+        public int characterDamage = 5;
+        public int characterArmor = 0;
+        public int characterEvaid = 1;
+    }
+}
